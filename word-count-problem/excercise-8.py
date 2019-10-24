79be3b381c3d6f6b3db5c8963f2baa3043fba2ee
Piotr Szyma <thompson2908@gmail.com>
2019-10-24 18:18:47 +0200

    str.title() on words that might be names of things

--- word-count-problem/excercise-8.py
+++ word-count-problem/excercise-8.py
@@ -10,20 +10,42 @@ import common
 MIN_SEN_LEN = 4
 MAX_SEN_LEN = 7
 
+to_be_uppercased = {
+    'lydia',
+    'i',
+    'james',
+    'jane',
+    'bingley',
+    'mr',
+    'ms',
+    'mrs',
+    'miss',
+    'bennet',
+    'wickham',
+    'wickhams',
+    'collins',
+    'darcy',
+    'lucas',
+    'elizabeth',
+}
+
 
 def get_random_sentence(word_to_most_common):
     sentence_len = random.randint(MIN_SEN_LEN, MAX_SEN_LEN)
     next_word = random.choice(tuple(word_to_most_common))
-    sentence = []
+    sentence = [next_word.title()]
 
-    for _ in range(sentence_len):
-        sentence.append(next_word)
+    for _ in range(sentence_len - 1):
         next_word = random.choice(tuple(word_to_most_common[next_word]))
+        sentence.append(next_word)
 
-    return ' '.join(sentence).capitalize() + '.'
+    sentence = ((word.title() if word in to_be_uppercased else word)
+                for word in sentence)
+
+    return ' '.join(sentence) + '.'
 
 
-def read_no_sentences_from_argv():
+def read_num_of_sentences_from_argv():
     try:
         return int(sys.argv[1])
     except (ValueError, IndexError):
@@ -31,7 +53,7 @@ def read_no_sentences_from_argv():
 
 
 def main():
-    no_sentences = read_no_sentences_from_argv() or 1
+    no_sentences = read_num_of_sentences_from_argv() or 1
 
     lines = common.read_lines_from_file("data/pride-and-prejudice.txt")
     words = tuple(common.extract_words_from_lines(lines))
@@ -48,7 +70,8 @@ def main():
         word_to_most_common[word] = tuple(w[0] for w in most_common)
 
     for _ in range(no_sentences):
-        print(get_random_sentence(word_to_most_common))
+        print(get_random_sentence(word_to_most_common), end=' ')
+    print()
 
 
 if __name__ == "__main__":
